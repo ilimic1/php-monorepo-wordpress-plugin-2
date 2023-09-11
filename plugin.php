0aad9b6e8700b46c9423de63b861d68e1080c169
Ivo Limić <ilimic1@gmail.com>
2023-09-11 15:09:59 +0200

    initial commit.

---
+++ plugin.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Plugin Name: Hello World 2
+Plugin URI: https://ilimic.from.hr
+Description: Dummy plugin which outputs hello in the admin, used for testing.
+Version: 1.0
+Author: Ivo Limić
+Author URI: https://ilimic.from.hr
+*/
+
+namespace Ilimic\Plugins\HelloWorld2;
+
+function hello_world() {
+	echo '<p>Hello World 2</p>';
+}
+add_action( 'admin_notices', __NAMESPACE__ . '\hello_world' );
