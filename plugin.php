1a6f6c0354d0274adfb09e258e21d503f22ff587
Ivo Limić <ilimic1@gmail.com>
2023-09-13 21:25:37 +0200

    adding puc.

--- plugin.php
+++ plugin.php
@@ -3,12 +3,24 @@
 Plugin Name: Hello World 2
 Plugin URI: https://ilimic.from.hr
 Description: Test plugin which outputs hello in the admin, used for testing.
-Version: 1.0.1
+Version: 1.0.3
 Author: Ivo Limić
 Author URI: https://ilimic.from.hr
 */
 
 namespace Ilimic\Plugins\HelloWorld2;
+
+use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$myUpdateChecker = PucFactory::buildUpdateChecker(
+	'https://github.com/ilimic1/php-monorepo-wordpress-plugin-2',
+	__FILE__, //Full path to the main plugin file or functions.php.
+	'ilimic/hello-world-2'
+);
+
+$myUpdateChecker->getVcsApi()->enableReleaseAssets();
 
 function hello_world() {
 	echo '<p>Hello World 2</p>';
